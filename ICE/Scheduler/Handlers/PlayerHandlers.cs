d1ed399c3cf86c25f0f61c5e23e56421961f4204
rioriopu <rioriopuu@gmail.com>
2026-09-22 21:57:10 +0900

    報酬画面がコールバックで閉じずループする問題にフォールバックを追加
    WKSReward がコールバック(-1)で閉じない事例があり、開いたままループして進行不能になる。
    報酬は完了時に確定済みのサマリー表示のため、3秒閉じなければ直接クローズする。
    報酬画面を隠す設定が有効な場合のみ動作するので、既定動作は変わらない。

--- ICE/Scheduler/Handlers/PlayerHandlers.cs
+++ ICE/Scheduler/Handlers/PlayerHandlers.cs
@@ -6,6 +6,7 @@ using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using System.Collections.Generic;
+using ICE.Utilities.Cosmic_Helper;
 using Callback = ECommons.Automation.Callback;
 using Time = (int start, int end);
 
@@ -14,6 +15,7 @@ namespace ICE.Scheduler.Handlers;
 internal static unsafe class PlayerHandlers
 {
     private static readonly uint stellarSprintID = 4398;
+    private static long _wksRewardSeenTick = 0; // WKSReward報酬ポップアップが開いた時刻(強制クローズ用)
 
     public static unsafe bool IsMoving()
     {
@@ -66,9 +68,23 @@ internal static unsafe class PlayerHandlers
                 {
                     GenericHandlers.FireCallback("WKSReward", true, -1);
                 }
+
+                // コールバック(-1)では閉じず WKSReward が開いたままになりループする事例へのフォールバック。
+                // 報酬は完了時に確定済みのサマリー表示なので、一定時間閉じなければ直接クローズして進行不能を防ぐ。
+                if (_wksRewardSeenTick == 0)
+                    _wksRewardSeenTick = Environment.TickCount64;
+                else if (Environment.TickCount64 - _wksRewardSeenTick > 3000 && EzThrottler.Throttle("Force closing reward popup", 1000))
+                {
+                    IceLogging.Warning("WKSReward報酬画面がコールバックで閉じないため、強制クローズします");
+                    addon->Close(true);
+                }
             }
         }
 
+        else
+        {
+            _wksRewardSeenTick = 0;
+        }
         if (C.StartUponEnterMoon)
         {
             if (PlayerHelper.IsInCosmicZone() && !PlayerFirstCosmicZone)
